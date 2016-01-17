c8e100386fcfb078b37e2b86d8f4ae5cda5f40a5
David Zhang <davzee@hotmail.com>
2016-01-17 01:27:12 -0500

    Added Braintree to checkout view

--- snapshop/Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ snapshop/Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -84,10 +84,12 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "Pods/Braintree.framework"
   install_framework "Pods/CNPPopupController.framework"
   install_framework "Pods/Firebase.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "Pods/Braintree.framework"
   install_framework "Pods/CNPPopupController.framework"
   install_framework "Pods/Firebase.framework"
 fi
--- snapshop/snapshop/CheckoutViewController.swift
+++ snapshop/snapshop/CheckoutViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import Braintree
 
-class CheckoutViewController: UIViewController {
-    
+class CheckoutViewController: UIViewController, BTDropInViewControllerDelegate {
+
+    var braintree: Braintree?
+
+    @IBOutlet var payButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        payButton.backgroundColor = UIColor(red: 0.5, green: 0.9, blue: 0.1, alpha: 1)
+        payButton.addTarget(self, action: "tappedPayButton", forControlEvents: UIControlEvents.TouchDown)
+        payButton.enabled = false
+        let clientTokenURL = NSURL(string: "https://braintree-sample-merchant.herokuapp.com/client_token")!
+        let clientTokenRequest = NSMutableURLRequest(URL: clientTokenURL)
+        clientTokenRequest.setValue("text/plain", forHTTPHeaderField: "Accept")
+        
+        NSURLSession.sharedSession().dataTaskWithRequest(clientTokenRequest) { (data, response, error) -> Void in
+            // TODO: Handle errors
+            let clientToken = String(data: data!, encoding: NSUTF8StringEncoding)
+            
+            // Initialize `Braintree` once per checkout session
+            self.braintree = Braintree(clientToken: clientToken!)
+            
+            self.payButton.enabled = true
+            // As an example, you may wish to present our Drop-in UI at this point.
+            // Continue to the next section to learn more...
+            }.resume()
         // Do any additional setup after loading the view.
     }
     
@@ -21,15 +43,35 @@ class CheckoutViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    
-    /*
-    // MARK: - Navigation
-    
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    // Get the new view controller using segue.destinationViewController.
-    // Pass the selected object to the new view controller.
+    func tappedPayButton() {
+        // If you haven't already, create and retain a `Braintree` instance with the client token.
+        // Typically, you only need to do this once per session.
+        //braintree = Braintree(clientToken: aClientToken)
+        
+        // Create a BTDropInViewController
+        let dropInViewController = braintree!.dropInViewControllerWithDelegate(self)
+        
+        // This is where you might want to customize your Drop-in. (See below.)
+        dropInViewController.view.tintColor = UIColor(red: 0, green: 0.8, blue: 0, alpha: 0.7)
+        // The way you present your BTDropInViewController instance is up to you.
+        // In this example, we wrap it in a new, modally presented navigation controller:
+        dropInViewController.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Cancel, target: self, action: "userDidCancelPayment")
+        
+        let navigationController = UINavigationController(rootViewController: dropInViewController)
+        self.presentViewController(navigationController, animated: true, completion: nil)
     }
-    */
+    
+    func userDidCancelPayment() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func dropInViewController(viewController: BTDropInViewController!, didSucceedWithPaymentMethod paymentMethod: BTPaymentMethod!) {
+        //postNonceToServer(paymentMethod.nonce) // Send payment method nonce to your server
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func dropInViewControllerDidCancel(viewController: BTDropInViewController!) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
     
 }
